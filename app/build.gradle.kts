07f8246eb9dc6be760e867bb752a9e5281cd29ed
sgrebenyk1 <sgrebenyk1@gmail.com>
2026-08-09 19:40:13 +0300

    build: remove unused moshi-kotlin-codegen dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -142,5 +142,4 @@ dependencies {
   debugImplementation(libs.androidx.compose.ui.test.manifest)
   debugImplementation(libs.androidx.compose.ui.tooling)
   "ksp"(libs.androidx.room.compiler)
-  "ksp"(libs.moshi.kotlin.codegen)
 }
